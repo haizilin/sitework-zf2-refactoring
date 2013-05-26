90ec5d70168b1b7c85a231b10df3729d0a8c2794
sitework <igor090>
2013-05-26 23:36:11 +0200

    much more sass

--- module/Project/src/Project/Controller/ProjectController.php
+++ module/Project/src/Project/Controller/ProjectController.php
@@ -12,16 +12,22 @@ class ProjectController extends AbstractActionController
         $prjModel = new Model\Project;
         $projects = $prjModel->getQuery()->limit(10)->find();
 
+        $sidebar = new ViewModel();
+        $sidebar->setVariable('projectsCollection', $projects);
+        $sidebar->setTemplate('project/partials/sidebar.project.teaser.phtml');
+        $this->layout()->addChild($sidebar, 'sidebar');
+
         $view = new ViewModel();
         $view->setVariable('projectsCollection', $projects);
-        $view->setTemplate('project/project/details.phtml');
+
         return $view;
     }
 
     public function detailsAction () {
         $prjModel = new Model\Project;
-        $sidebar = new ViewModel();
         $projects = $prjModel->getQuery()->find();
+
+        $sidebar = new ViewModel();
         $sidebar->setVariable('projectsCollection', $projects);
         $sidebar->setTemplate('project/partials/sidebar.project.teaser.phtml');
         $this->layout()->addChild($sidebar, 'sidebar');
